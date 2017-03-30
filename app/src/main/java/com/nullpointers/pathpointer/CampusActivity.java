af0f7dd46f2c487112dbcfec7aee23f365678fb9
jfucci <jacfu777@gmail.com>
2017-03-30 03:23:18 -0400

    Code to send selected building id's to the CampusActivity and use them to draw a path

--- app/src/main/java/com/nullpointers/pathpointer/CampusActivity.java
+++ app/src/main/java/com/nullpointers/pathpointer/CampusActivity.java
@@ -1,5 +1,6 @@
 package com.nullpointers.pathpointer;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -11,33 +12,48 @@ import android.os.Bundle;
 
 import com.ortiz.touch.TouchImageView;
 
+import java.util.List;
+
 public class CampusActivity extends AppCompatActivity {
 
     TouchImageView campusView;
+    private static final int originalImageWidth = 2175;
+    private static final int originalImageHeight = 2055;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campus);
+        Intent intent = getIntent();
+
+        Campus campus = new Campus(this, "Data/Nodes", "Data/Edges");
+        int sourceBuilding = intent.getIntExtra("sourceBuilding", 1);
+        int destinationBuilding = intent.getIntExtra("destinationBuilding", 2);
+
+        List<List<Location>> path = campus.getShortestPath(sourceBuilding, destinationBuilding);
+        List<Location> campusPath = path.get(0);
+
 
         campusView = (TouchImageView)findViewById(R.id.campusView);
         Resources res = getResources();
         Bitmap imgBitmap = BitmapFactory.decodeResource(res, R.drawable.campusmap);
-
         Bitmap cBitmap = imgBitmap.copy(Bitmap.Config.ARGB_8888, true);
         Canvas canvas = new Canvas(cBitmap);
-
-        // Line
-        drawLine(canvas, Color.GREEN, 900, 1000, 900, 800);
-        drawLine(canvas, Color.GREEN, 900, 800, 1600, 800);
-
         campusView.setImageBitmap(cBitmap);
+
+        for (int i = 1; i < campusPath.size(); i++) {
+            drawPath(canvas, Color.GREEN, campusPath.get(i - 1).getX(), campusPath.get(i - 1).getY(), campusPath.get(i).getX(), campusPath.get(i).getY());
+        }
     }
 
-    private void drawLine(Canvas canvas, int color, int startx, int starty, int endx, int endy) {
+    private void drawPath(Canvas canvas, int color, double startx, double starty, double endx, double endy) {
+        double viewWidth = campusView.getDrawable().getIntrinsicWidth();
+        double viewHeight = campusView.getDrawable().getIntrinsicHeight();
+        double xscale = viewWidth / originalImageWidth;
+        double yscale = viewHeight / originalImageHeight;
         Paint paint = new Paint();
         paint.setColor(color);
         paint.setStrokeWidth(10);
-        canvas.drawLine(startx, starty, endx, endy, paint);
+        canvas.drawLine((float)(startx*xscale), (float)(starty*yscale), (float)(endx*xscale), (float)(endy*yscale), paint);
     }
 }
--- app/src/main/java/com/nullpointers/pathpointer/MainActivity.java
+++ app/src/main/java/com/nullpointers/pathpointer/MainActivity.java
@@ -4,17 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner sourceBuildingSpinner;
+    private Spinner destinationBuildingSpinner;
+
     private static class StringWithTag {
         public String string;
         public Object tag;
@@ -30,64 +31,36 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    Campus campus;
-    Map<String, Integer> buildingMap;
-    Spinner sourceBuildingSpinner;
-    Spinner destinationBuildingSpinner;
-    ArrayAdapter<CharSequence> buildingAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        campus = new Campus(this, "Data/Nodes", "Data/Edges");
-        buildingMap = new HashMap<String, Integer>();
-        buildingMap = campus.getBuildings();
+        Campus campus = new Campus(this, "Data/Nodes", "Data/Edges");
+        Map<String, Integer> buildingMap = campus.getBuildings();
         sourceBuildingSpinner = (Spinner)findViewById(R.id.spinner1);
         destinationBuildingSpinner = (Spinner)findViewById(R.id.spinner2);
-        List<StringWithTag> buildingList = new ArrayList<StringWithTag>();
+        List<StringWithTag> buildingList = new ArrayList<>();
         for(Map.Entry<String,Integer> entry : buildingMap.entrySet()) {
             String key = entry.getKey();
             Integer value = entry.getValue();
 
             buildingList.add(new StringWithTag(key, value));
         }
-        ArrayAdapter<StringWithTag> buildingAdapter = new ArrayAdapter<StringWithTag>(this, android.R.layout.simple_spinner_item, buildingList);
+        ArrayAdapter<StringWithTag> buildingAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, buildingList);
         buildingAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
         sourceBuildingSpinner.setAdapter(buildingAdapter);
         destinationBuildingSpinner.setAdapter(buildingAdapter);
-
-        sourceBuildingSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                StringWithTag sourceBuilding = (StringWithTag) parent.getItemAtPosition(position);
-                Integer sourceKey = (Integer) sourceBuilding.tag;
-                Toast.makeText(getBaseContext(), sourceKey + " selected.", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        destinationBuildingSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                StringWithTag destinationBuilding = (StringWithTag) parent.getItemAtPosition(position);
-                Integer destinationKey = (Integer) destinationBuilding.tag;
-                Toast.makeText(getBaseContext(), destinationKey + " selected.", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
     }
 
     public void viewCampusActivity(View view) {
         Intent intent = new Intent(this, CampusActivity.class);
+        StringWithTag selectedItem = (StringWithTag)(sourceBuildingSpinner).getSelectedItem();
+        int sourceBuilding = (int)selectedItem.tag;
+        intent.putExtra("sourceBuilding", sourceBuilding);
+        selectedItem = (StringWithTag)(destinationBuildingSpinner).getSelectedItem();
+        int destinationBuilding = (int)selectedItem.tag;
+        intent.putExtra("destinationBuilding", destinationBuilding);
         startActivity(intent);
     }
 }
