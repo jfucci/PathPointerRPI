795dcd35df382c19220b589da1568cc7949130d2
dobbim <dobbim@rpi.edu>
2017-03-30 12:52:15 -0400

    Added skeleton methods for mutability functions

--- app/src/main/java/com/nullpointers/pathpointer/Building.java
+++ app/src/main/java/com/nullpointers/pathpointer/Building.java
@@ -19,15 +19,15 @@ public class Building {
 
     private Integer id;
     private String name;
-    private Map<FacilityType, Set<Facility>> facilities;
     private Set<Room> rooms;
+    private Map<FacilityType, Set<Facility>> facilities;
 
     /** Constructs a null Building. */
     public Building() {
         id = null;
         name = null;
-        facilities = new HashMap<>();
         rooms = new HashSet<>();
+        facilities = new HashMap<>();
     }
 
     /**
@@ -70,6 +70,66 @@ public class Building {
     }
 
     /**
+     * Ensures that this Building contains the specified Room
+     * @param r The Room to add to the Building.
+     * @return True if the collection of Rooms in this Building changed due to this call
+     */
+    public boolean add(Room r) {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /**
+     * Ensures that this Building contains the specified Facility
+     * @param f The Facility to add to the Building.
+     * @return True if the collection of Facilities in this Building changed due to this call
+     */
+    public boolean add(Facility f) {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /**
+     * Ensures that this Building contains each Room in the specified Collection
+     * @param r The Collection of Rooms to add to the Building.
+     * @return True if the collection of Rooms in this Building changed due to this call
+     */
+    public boolean addAllRooms(Collection<? extends Room> r) {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /**
+     * Ensures that this Building contains each Facility in the specified Collection
+     * @param f The Collection of Facilities to add to the Building.
+     * @return True if the collection of Facilities in this Building changed due to this call
+     */
+    public boolean addAllFacilities(Collection<? extends Facility> f) {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /** Removes all Rooms and Facilities from this Building */
+    public void clear() {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /** Removes all Rooms from this Building; Facilities are unchanged */
+    public void clearRooms() {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /** Removes all Facilities from this Building; Rooms are unchanged */
+    public void clearFacilities() {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /**
+     * Checks whether or not this Building contains the specified Room.
+     * @param room The Room which may or may not be located in this Building
+     * @return true if this Building contains the specified Room, false otherwise
+     */
+    public boolean contains(Room room) {
+        return rooms.contains(room);
+    }
+
+    /**
      * Checks whether or not this Building contains the specified Facility.
      * @param facility The Facility which may or may not be located in this Building
      * @return true if this Building contains the specified Facility, false otherwise
@@ -82,13 +142,9 @@ public class Building {
         }
     }
 
-    /**
-     * Checks whether or not this Building contains the specified Room.
-     * @param room The Room which may or may not be located in this Building
-     * @return true if this Building contains the specified Room, false otherwise
-     */
-    public boolean contains(Room room) {
-        return rooms.contains(room);
+    /** Returns the number of Rooms contained in this Building */
+    public int countRooms() {
+        return rooms.size();
     }
 
     /** Returns the number of Facilities of all types contained in this Building */
@@ -116,15 +172,15 @@ public class Building {
         }
     }
 
-    /** Returns the number of Rooms contained in this Building */
-    public int countRooms() {
-        return rooms.size();
+    /** Returns an immutable Iterator which can be used to traverse all Rooms in this Building */
+    public Iterator<Room> roomIterator() {
+        return Collections.unmodifiableSet(rooms).iterator();
     }
 
     /**
      * @param type The type of Facility to iterate over
-     * @return An Iterator which can be used to traverse all Facilities of specified type in this
-     * Building
+     * @return An immutable Iterator which can be used to traverse all Facilities of specified type
+     * in this Building
      */
     public Iterator<Facility> facilityIterator(FacilityType type) {
         if (facilities.containsKey(type)) {
@@ -137,9 +193,62 @@ public class Building {
         }
     }
 
-    /** Returns an Iterator which can be used to traverse all Rooms in this Building */
-    public Iterator<Room> roomIterator() {
-        return Collections.unmodifiableSet(rooms).iterator();
+    /**
+     * Removes a single instance of the specified Room from this Building, if it is present.
+     * @param r The Room to remove
+     * @return True if the collection of Rooms in this Building changed due to this call
+     */
+    boolean remove(Room r) {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /**
+     * Removes a single instance of the specified Facility from this Building, if it is present.
+     * @param f The Facility to remove
+     * @return True if the collection of Facilities in this Building changed due to this call
+     */
+    boolean remove(Facility f) {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /**
+     * Removes all instances of each of the specified Rooms from this Building.  Any room [o] which
+     * satisfies r.contains(o) == true will be removed.
+     * @param r The Collection of Rooms to remove
+     * @return True if the collection of Rooms in this Building changed due to this call
+     */
+    boolean removeAllRooms(Collection<? extends Room> r) {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /**
+     * Removes all instances of each of the specified Facilities from this Building.  Any facility
+     * [o] which satisfies f.contains(o) == true will be removed.
+     * @param f The Collection of Facilities to remove
+     * @return True if the collection of Facilities in this Building changed due to this call
+     */
+    boolean removeAllFacilities(Collection<? extends Facility> f) {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /**
+     * Retains only instances of each of the specified Rooms from this Building.  Any room which
+     * satisfies r.contains(o) == false will be removed.
+     * @param r The Collection of Rooms to retain
+     * @return True if the collection of Rooms in this Building changed due to this call
+     */
+    boolean retainAllRooms(Collection<? extends Room> r) {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /**
+     * Retains only instances of each of the specified Facilities from this Building.  Any facility
+     * [o] which satisfies f.contains(o) == false will be removed.
+     * @param f The Collection of Facilities to retain
+     * @return True if the collection of Facilities in this Building changed due to this call
+     */
+    boolean retainAllFacilities(Collection<? extends Facility> f) {
+        throw new RuntimeException("Method not implemented");
     }
 
 }
