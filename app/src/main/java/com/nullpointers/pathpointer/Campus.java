f462b7ddcbe5c4350d392f6476e8b7d128601bdc
ferria <ferria@rpi.edu>
2017-04-15 14:04:19 -0400

    Fixed Campus

--- app/src/main/java/com/nullpointers/pathpointer/Campus.java
+++ app/src/main/java/com/nullpointers/pathpointer/Campus.java
@@ -26,10 +26,13 @@ import org.jgrapht.graph.SimpleWeightedGraph;
  */
 
 public class Campus {
-    Map<Integer, Building> buildings;
-    Map<String, Integer> buildingsNameToId;
-    Graph<Location, DefaultWeightedEdge> campusGraph;
-    Map<Integer, Location> locations;
+    //The amount that will be added to the ID's of each floorplan
+    public static final int ID_MOD = 1000000;
+
+    private Map<Integer, Building> buildings;
+    private Map<String, Integer> buildingsNameToId;
+    private Graph<Location, DefaultWeightedEdge> campusGraph;
+    private Map<Integer, Location> locations;
 
     /**
      * Makes a new Campus object that will take from the given nodes and edge folders
@@ -57,12 +60,32 @@ public class Campus {
         }
 
         if (nodeFiles != null) {
+            //Find all campus files (X.0.0.csv), and add the buildings from them
+            for(String nodeFile : nodeFiles) {
+                String[] fileNameElements = nodeFile.split("\\.");
+                int building = Integer.parseInt(fileNameElements[1]);
+                if(building == 0) {
+                    InputStream nodeFileStream = null;
+                    try {
+                        nodeFileStream = assetManager.open(nodesFolder + '/' + nodeFile);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if(nodeFileStream != null) {
+                        addBuildings(nodeFileStream);
+                    }
+                }
+            }
             for (String nodeFile : nodeFiles) {
                 int floorplan;
+                int buildingId;
                 try {
-                    floorplan = Integer.parseInt(nodeFile.split("\\.")[0]);
+                    String[] fileNameElements = nodeFile.split("\\.");
+                    floorplan = Integer.parseInt(fileNameElements[0]);
+                    buildingId = Integer.parseInt(fileNameElements[1]);
                 } catch(NumberFormatException nfe) {
                     floorplan = -1;
+                    buildingId = -1;
                 }
 
                 InputStream nodeFileStream = null;
@@ -72,7 +95,7 @@ public class Campus {
                     e.printStackTrace();
                 }
                 if(nodeFileStream != null) {
-                    addNodes(nodeFileStream, floorplan);
+                    addNodes(nodeFileStream, floorplan, buildingId);
                 }
             }
         } else {
@@ -81,13 +104,6 @@ public class Campus {
 
         if (edgeFiles != null) {
             for (String edgeFile : edgeFiles) {
-                int floorplan;
-                try {
-                    floorplan = Integer.parseInt(edgeFile.split("\\.")[0]);
-                } catch(NumberFormatException nfe) {
-                    floorplan = -1;
-                }
-
                 InputStream edgeFileStream = null;
                 try {
                     edgeFileStream = assetManager.open(edgesFolder + '/' + edgeFile);
@@ -95,7 +111,13 @@ public class Campus {
                     e.printStackTrace();
                 }
                 if(edgeFileStream != null) {
-                    addEdges(edgeFileStream);
+                    String[] fileNameElements = edgeFile.split("\\.");
+                    if(fileNameElements[0].equals("X")) {
+                        addEdgesCross(edgeFileStream);
+                    }
+                    else {
+                        addEdges(edgeFileStream, Integer.parseInt(fileNameElements[0]));
+                    }
                 }
             }
         } else {
@@ -104,35 +126,71 @@ public class Campus {
     }
 
     /**
-     * Add the nodes from the nodeInput file to the graph
-     * @param nodeInput the file from which to retrieve the nodes
-     * @param floorplan the floorplan of all the nodes being added
+     * Add all buildings to the building map
+     * @param nodeInput a campus node input file
      */
-    private void addNodes(InputStream nodeInput, int floorplan) {
+    private void addBuildings(InputStream nodeInput) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(nodeInput))) {
             String nextLocation;
             while ((nextLocation = br.readLine()) != null) {
                 String[] details = nextLocation.split(",");
                 String buildingName = details[0];
                 int buildingID = Integer.parseInt(details[1]);
+
+                if(!buildingName.equals("")) {
+                    Set<Room> rooms = new HashSet<>();
+                    Building newBuilding = new Building(null, rooms, buildingName, buildingID);
+                    buildings.put(buildingID, newBuilding);
+                    buildingsNameToId.put(buildingName, buildingID);
+                }
+            }
+        }
+        catch (FileNotFoundException fnfe) {
+            System.out.println("Invalid nodes file");
+        } catch (IOException io) {
+            System.out.println("IOException while opening file in addNodes");
+        }
+    }
+
+    /**
+     * Add the nodes from the nodeInput file to the graph
+     * @param nodeInput the file from which to retrieve the nodes
+     * @param floorplan the floorplan of all the nodes being added
+     * @param buildingId the building that these nodes are in
+     */
+    private void addNodes(InputStream nodeInput, int floorplan, int buildingId) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(nodeInput))) {
+            String nextLocation;
+            while ((nextLocation = br.readLine()) != null) {
+                String[] details = nextLocation.split(",");
+                String locationName = details[0];
+                int locationID = Integer.parseInt(details[1]);
+                int modified_ID = (floorplan * ID_MOD) + locationID;
                 double xCoord = Integer.parseInt(details[2]);
                 double yCoord = Integer.parseInt(details[3]);
-                if(!buildingName.equals("")) {  //ignore intersections
-                    Room room = new Room(buildingID, floorplan, xCoord, yCoord, buildingName, buildingName);
-                    Set<Room> rooms = new HashSet<>();
-                    rooms.add(room);
-                    if(floorplan == 0) {
-                        Building building = new Building(null, rooms, buildingName, buildingID);
-                        buildings.put(buildingID, building);
-                        buildingsNameToId.put(buildingName, buildingID);
+                if(!locationName.equals("")) {  //ignore intersections
+                    Building building;
+                    Room room;
+                    if(buildingId == 0) {  //This is a building on campus
+                        //Add the "overall building" room
+                        building = buildings.get(locationID);
+                        room = new Room(modified_ID, floorplan, xCoord, yCoord,
+                                locationName, building.getName());
                     }
+                    else {
+                        building = buildings.get(buildingId);
+                        room = new Room(modified_ID, floorplan,
+                                xCoord, yCoord, locationName,
+                                building.getName());
+                    }
+                    building.add(room);
                     campusGraph.addVertex(room);
-                    locations.put(buildingID, room);
+                    locations.put(modified_ID, room);
                 }
                 else {
-                    Intersection intersection = new Intersection(buildingID, floorplan, xCoord, yCoord);
+                    Intersection intersection = new Intersection(modified_ID, floorplan, xCoord, yCoord);
                     campusGraph.addVertex(intersection);
-                    locations.put(buildingID, intersection);
+                    locations.put(modified_ID, intersection);
                 }
             }
         } catch (FileNotFoundException fnfe) {
@@ -146,14 +204,17 @@ public class Campus {
      * Add edges to the graph.
      * @param edgeInput the file from which to get the edge data
      */
-    private void addEdges(InputStream edgeInput) {
+    private void addEdges(InputStream edgeInput, int floorplan) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(edgeInput))) {
             String nextEdge;
-            while((nextEdge = br.readLine()) != null) {
+            while ((nextEdge = br.readLine()) != null) {
                 String[] details = nextEdge.split(",");
-                int firstLocation = Integer.parseInt(details[0]);
-                int secondLocation = Integer.parseInt(details[1]);
-                campusGraph.addEdge(locations.get(firstLocation), locations.get(secondLocation));
+                int firstLocation = Integer.parseInt(details[0]) + (floorplan * ID_MOD);
+                int secondLocation = Integer.parseInt(details[1]) + (floorplan * ID_MOD);
+                DefaultWeightedEdge edge =
+                        campusGraph.addEdge(locations.get(firstLocation), locations.get(secondLocation));
+                campusGraph.setEdgeWeight(edge,
+                        getDistance(locations.get(firstLocation), locations.get(secondLocation)));
             }
         } catch (FileNotFoundException fnfe) {
             System.out.println("Invalid edge file");
@@ -163,10 +224,49 @@ public class Campus {
     }
 
     /**
+     * Add edges from a cross-floorplan edge file
+     * @param edgeInput the file that contains cross-floorplan edges
+     */
+    private void addEdgesCross(InputStream edgeInput) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(edgeInput))) {
+            String nextEdge;
+            while ((nextEdge = br.readLine()) != null) {
+                String[] details = nextEdge.split(",");
+                int firstFloorplan = Integer.parseInt(details[0]);
+                int firstLocation = Integer.parseInt(details[1]);
+                int secondFloorplan = Integer.parseInt(details[2]);
+                int secondLocation = Integer.parseInt(details[3]);
+                double distance = Integer.parseInt(details[4]);
+
+                firstLocation = firstLocation + (firstFloorplan * ID_MOD);
+                secondLocation = secondLocation + (secondFloorplan * ID_MOD);
+
+                DefaultWeightedEdge edge =
+                        campusGraph.addEdge(locations.get(firstLocation), locations.get(secondLocation));
+                campusGraph.setEdgeWeight(edge, distance);
+            }
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("Invalid edge file");
+        } catch (IOException io) {
+            System.out.println("IOException while opening file in addEdges");
+        }
+    }
+
+    /**
+     * Get the distance between two locations
+     * @param l1 the first location
+     * @param l2 the second location
+     * @return the straight-line distance between these locations.
+     */
+    private double getDistance(Location l1, Location l2) {
+        return Math.sqrt(Math.pow(l1.getX() - l2.getX(), 2) + Math.pow(l1.getY() - l2.getY(), 2));
+    }
+
+    /**
      * @return a map of all buildings mapped to their IDs
      */
     public Map<String, Integer> getBuildings() {
-        return buildingsNameToId;
+        return new HashMap<>(buildingsNameToId);
     }
 
     /**
@@ -198,11 +298,13 @@ public class Campus {
         DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(campusGraph);
         GraphPath shortestPath = dijkstraShortestPath.getPath(startLocation, endLocation);
         List<Location> path = shortestPath.getVertexList();
+        List<List<Location>> segmentedPath = new ArrayList<>();
         if(path.size() == 0) {
             System.out.println("No path exists");
-            return null;
+            List<Location> empty = new ArrayList<>();
+            segmentedPath.add(empty);
+            return segmentedPath;
         }
-        List<List<Location>> segmentedPath = new ArrayList<>();
         int currentFloorplan = path.get(0).getFloorPlan();
         int i = 0;
         while(i < path.size()) {
@@ -212,6 +314,9 @@ public class Campus {
                 i++;
             }
             segmentedPath.add(nextSegment);
+            if(i < path.size()) {
+                currentFloorplan = path.get(i).getFloorPlan();
+            }
         }
         return segmentedPath;
     }
